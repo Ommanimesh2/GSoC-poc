c4148dfd6624481aa526fb71d2e45bceee924322
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2023-04-04 03:49:28 +0530

    feat: Removes print functions

--- src/core/api.py
+++ src/core/api.py
@@ -5,7 +5,7 @@ import requests
 class ApiBase:
     def __init__(self, base_url):
         self.base_url = base_url
-        print("this is called")
+
 
     def get(self,endpoint, params=None):
         url = f"{self.base_url}/{endpoint}"
