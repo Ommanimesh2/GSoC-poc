4e53efc91c2c434bd7ea54dc8880707d3612ffae
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2023-04-04 10:10:48 +0530

    feat: Adds unit tests

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 setup(
     name='ExtensibleAPIClient',
-    packages=find_packages(),
     version='0.1.0',
     description='Made for OGC apis',
-    author='Me',
+    author='Omm',
     license='MIT',
+    
+    packages=find_packages(),
+    install_requires=["argparser", "requests"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["genApi=src.cli"]},
 )
---
+++ tests/test_client.py
@@ -0,0 +1,25 @@
+import unittest
+from src.demoapi.api import DemoAPi
+
+class TestDummyJsonClient(unittest.TestCase):
+    def setUp(self):
+        self.client = DemoAPi()
+
+    def test_get_products(self):
+        products = self.client.products.get()
+        self.assertIsInstance(products, list)
+
+    def test_get_product(self):
+        product = self.client.products.get(1)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product['id'], 1)
+
+    def test_get_todos(self):
+        todos = self.client.todo.get()
+        self.assertIsInstance(todos, dict)
+        self.assertEqual(todos, list)
+
+    def test_get_product(self):
+        todo = self.client.products.get(1)
+        self.assertIsInstance(todo, dict)
+        self.assertEqual(todo['id'], 1)
